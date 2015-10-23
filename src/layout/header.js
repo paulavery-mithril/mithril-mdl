e494cab7c4715ec048f945976196429304e8508c
Boaz Yaniv <boazyan@gmail.com>
2015-10-23 15:34:38 +0900

    Add is-casting-shadow to Layout Header.

--- src/layout/header.js
+++ src/layout/header.js
@@ -5,13 +5,14 @@ export let Header = {
 	view(ctrl, args, ...children) {
 		args = args || {};
 		let attr = attributes(args);
-		let {scroll, waterfall, transparent} = args;
+		let {scroll, waterfall, transparent, castShadow} = args;
 
 		attr.class.push('mdl-layout__header');
 
 		if(scroll) attr.class.push('mdl-layout__header--scroll');
 		if(waterfall) attr.class.push('mdl-layout__header--waterfall');
 		if(transparent) attr.class.push('mdl-layout__header--transparent');
+		if(castShadow) attr.class.push('is-casting-shadow');
 
 		return <header {...attr}>{children}</header>;
 	}
