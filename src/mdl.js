1ab1dcda6936bd6ebfe2f4ce5f8a671c80cfb7d6
Sebastien Renault <srenault.contact@gmail.com>
2015-12-01 08:20:24 +0100

    Add support for menu

--- src/mdl.js
+++ src/mdl.js
@@ -3,6 +3,7 @@ export {default as attributes} from './attributes';
 export {Icon, Spacer} from './misc';
 export {Layout, Header, HeaderRow, Title, Tabs, Tab, Drawer, Navigation, NavLink, Content} from './layout';
 export {Button, Fab} from './button';
+export {Menu, MenuItem} from './menu';
 export {Grid, Cell} from './grid';
 export {ProgressBar} from './loading';
 export {TextInput} from './input';
