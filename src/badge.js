1da86d69eb0d12790ec3172ecae2588e0c000047
Sebastien Renault <srenault.contact@gmail.com>
2015-12-01 21:17:07 +0100

    Add support for badge

---
+++ src/badge.js
@@ -0,0 +1,15 @@
+import m from 'mithril';
+import attributes from './attributes';
+
+export let Badge = {
+    view(ctrl, args, label) {
+        args = args || {};
+        let attr = attributes(args);
+        let {badge, icon} = args;
+
+        attr.class.push('mdl-badge');
+        if(icon) attr.class.push('material-icons');
+
+        return <span {...attr} data-badge={badge}>{icon ? icon : label}</span>;
+    }
+};
