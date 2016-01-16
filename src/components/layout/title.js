213c76858a501ae892d3aad2e6cc2475a0e794ab
Florian Albertz <florian@albertz.io>
2016-01-16 18:07:08 +0100

    Change element type of title

--- src/components/layout/title.js
+++ src/components/layout/title.js
@@ -1,4 +1,4 @@
 import {mixin, fromMixin} from '../../mixin';
 
 export let title = mixin('title', 'mdl-layout-title');
-export let Title = fromMixin(title);
+export let Title = fromMixin(title, 'span');
