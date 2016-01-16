f9b75098290995896863d0eb774dcb6ea71f91f9
Florian Albertz <florian@albertz.io>
2016-01-16 18:06:57 +0100

    Change mixin name of spacer

--- src/components/layout/spacer.js
+++ src/components/layout/spacer.js
@@ -1,4 +1,4 @@
 import {mixin, fromMixin} from '../../mixin';
 
-export let spacer = mixin('header', 'mdl-layout-spacer');
+export let spacer = mixin('spacer', 'mdl-layout-spacer');
 export let Spacer = fromMixin(spacer);
