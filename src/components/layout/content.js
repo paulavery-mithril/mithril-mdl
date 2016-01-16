f03d6f1623ebfdc75d56cf16ec1e0aafa208b1e3
Florian Albertz <florian@albertz.io>
2016-01-16 17:59:41 +0100

    Fix tagtype of content

--- src/components/layout/content.js
+++ src/components/layout/content.js
@@ -1,4 +1,4 @@
 import {mixin, fromMixin} from '../../mixin';
 
 export let content = mixin('content', 'mdl-layout__content');
-export let Content = fromMixin(content);
+export let Content = fromMixin(content, 'main');
