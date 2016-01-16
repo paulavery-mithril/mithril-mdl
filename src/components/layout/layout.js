d62e2a84cf2fc2d98a609efe9a8808ed39aefe76
Florian Albertz <florian@albertz.io>
2016-01-16 18:05:03 +0100

    Remove drawer option from layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,11 @@
 import {mixin, fromMixin} from '../../mixin';
 
-export let layout = mixin('layout', function({drawer, fixedDrawer, fixedHeader, fixedTabs}) {
+export let layout = mixin('layout', function({fixedDrawer, fixedHeader, fixedTabs}) {
 	this.class.push('mdl-layout mdl-js-layout');
 
 	if(fixedTabs) this.class.push('mdl-layout--fixed-tabs');
 	if(fixedDrawer) this.class.push('mdl-layout--fixed-drawer');
 	if(fixedHeader) this.class.push('mdl-layout--fixed-header');
-	if(drawer || fixedDrawer) this.class.push('mdl-layout--overlay-drawer-button');
 });
 
 export let Layout = fromMixin(layout);
